91e929fa6d795edf8778a3b984f46f337eab4dbe
wipdata <borhan@wipdata.com>
2023-01-03 17:17:50 +0600

    added new sections ain new landing page

--- src/screens/AcevaScreen.js
+++ src/screens/AcevaScreen.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import Header from '../components/header/Header'
-import Footer from '../components/footer'
-import Hero_Section from '../components/aceva-landing-page/Hero_Section'
-import Column_Section from '../components/aceva-landing-page/Column_Section'
 import Brand_Section from '../components/aceva-landing-page/Brand_Section'
+import Column_Section from '../components/aceva-landing-page/Column_Section'
 import Column_Section2 from '../components/aceva-landing-page/Column_Section2'
 import Column_Section3 from '../components/aceva-landing-page/Column_Section3'
+import Hero_Section from '../components/aceva-landing-page/Hero_Section'
 import Integrate_Section from '../components/aceva-landing-page/Integrate_Section'
 import Why_Choose_Section from '../components/aceva-landing-page/Why_Choose_Section'
+import Footer from '../components/footer'
+import Header from '../components/header/Header'
+import FaqSection from '../components/home-page/FaqSection'
+import FeaturedAreaSection from '../components/home-page/featured-section/FeaturedSection'
+import NewsLetterSection from '../components/home-page/NewsLetterSection'
+import TestimonialSection from '../components/home-page/TestimonialSection'
+import WhatWeDoSection from '../components/home-page/WhatWeDoSection'
+import Chatbot from '../components/chatbot/Chatbot'
 
 function AcevaScreen() {
   return (
     <>
+      <Chatbot />
       <Header />
         <Hero_Section />
         <Brand_Section />
@@ -20,6 +27,11 @@ function AcevaScreen() {
         <Column_Section3 />
         <Integrate_Section />
         <Why_Choose_Section />
+        <FeaturedAreaSection />
+        <WhatWeDoSection />
+        <TestimonialSection />
+        <FaqSection />
+        <NewsLetterSection />
       <Footer />
     </>
   )
